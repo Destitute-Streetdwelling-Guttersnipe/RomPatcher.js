d7cdaf7efef93feb9d1e681b4f34463308c85bc4
DSG (Destitute Streetdwelling Guttersnipe) <90743025+Destitute-Streetdwelling-Guttersnipe@users.noreply.github.com>
2023-08-21 11:47:05 +0700

    use switch-patch-file-in-url to choose saving patch in file or in url

--- js/RomPatcher.js
+++ js/RomPatcher.js
@@ -464,7 +464,7 @@ addEvent(window,'load',function(){
 		// enable app setting 'patchFileInUrl' when clicking on a 'disabled' switch
 		AppSettings.patchFileInUrl = this.className==='switch disabled';
 		this.className = AppSettings.patchFileInUrl ? 'switch enabled' : 'switch disabled';
-		el('patch-file-in-url').style.display = 'none';
+		el('patch-file-in-url').style.display = 'inline';
 		AppSettings.save();
 	});
 
@@ -964,12 +964,16 @@ function setCreatorMode(creatorMode){
  */
 var saveAs=saveAs||function(c){"use strict";if(!(void 0===c||"undefined"!=typeof navigator&&/MSIE [1-9]\./.test(navigator.userAgent))){var t=c.document,f=function(){return c.URL||c.webkitURL||c},s=t.createElementNS("http://www.w3.org/1999/xhtml","a"),d="download"in s,u=/constructor/i.test(c.HTMLElement)||c.safari,l=/CriOS\/[\d]+/.test(navigator.userAgent),p=c.setImmediate||c.setTimeout,v=function(t){p(function(){throw t},0)},w=function(t){setTimeout(function(){"string"==typeof t?f().revokeObjectURL(t):t.remove()},4e4)},m=function(t){return/^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(t.type)?new Blob([String.fromCharCode(65279),t],{type:t.type}):t},r=function(t,n,e){e||(t=m(t));var r,o=this,a="application/octet-stream"===t.type,i=function(){!function(t,e,n){for(var r=(e=[].concat(e)).length;r--;){var o=t["on"+e[r]];if("function"==typeof o)try{o.call(t,n||t)}catch(t){v(t)}}}(o,"writestart progress write writeend".split(" "))};if(o.readyState=o.INIT,d)return r=f().createObjectURL(t),void p(function(){var t,e;s.href=r,s.download=n,t=s,e=new MouseEvent("click"),t.dispatchEvent(e),i(),w(r),o.readyState=o.DONE},0);!function(){if((l||a&&u)&&c.FileReader){var e=new FileReader;return e.onloadend=function(){var t=l?e.result:e.result.replace(/^data:[^;]*;/,"data:attachment/file;");c.open(t,"_blank")||(c.location.href=t),t=void 0,o.readyState=o.DONE,i()},e.readAsDataURL(t),o.readyState=o.INIT}r||(r=f().createObjectURL(t)),a?c.location.href=r:c.open(r,"_blank")||(c.location.href=r);o.readyState=o.DONE,i(),w(r)}()},e=r.prototype;return"undefined"!=typeof navigator&&navigator.msSaveOrOpenBlob?function(t,e,n){return e=e||t.name||"download",n||(t=m(t)),navigator.msSaveOrOpenBlob(t,e)}:(e.abort=function(){},e.readyState=e.INIT=0,e.WRITING=1,e.DONE=2,e.error=e.onwritestart=e.onprogress=e.onwrite=e.onabort=e.onerror=e.onwriteend=null,function(t,e,n){return new r(t,e||t.name||"download",n)})}}("undefined"!=typeof self&&self||"undefined"!=typeof window&&window||this);
 
-if (AppSettings.patchFileInUrl) {
-	saveAs = function(blob, fileName) {
-		var PATCHER = [{ name: fileName, file: 'data:;base64,' + base64_arraybuffer(blob) }]
-		el('patch-file-in-url').value = '#' + JSON.stringify({PATCHER: PATCHER})
-		el('patch-file-in-url').style.display = 'block'
+const realSaveAs = saveAs
+saveAs = function(blob, fileName) {
+	if (!AppSettings.patchFileInUrl) {
+		return realSaveAs(blob, fileName)
 	}
+	return base64_arraybuffer(blob).then(function(dataInBase64) {
+		const PATCHER = [{ name: fileName, file: 'data:;base64,' + dataInBase64 }]
+		el('patch-file-in-url').value = '#' + JSON.stringify({PATCHER: PATCHER})
+		el('patch-file-in-url').style.display = 'inline'
+	})
 }
 
 // ref: ArrayBufferToBase64.js https://gist.github.com/fonsp/a4a2d127f35e72deec14227c1bdf3d04
